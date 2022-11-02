f96916a7c0f27a8543a282cc421e627dda1668c1
Jeet214 <98124523+Jeet214@users.noreply.github.com>
2022-11-01 21:50:55 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAOwMXKzazXPvJlMI7YuRvZ0q8efCDGN3c",
+    authDomain: "auth-implemented.firebaseapp.com",
+    projectId: "auth-implemented",
+    storageBucket: "auth-implemented.appspot.com",
+    messagingSenderId: "201333894254",
+    appId: "1:201333894254:web:bb7af43735d15d7aa78a7f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
